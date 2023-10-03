be378b53d2f7fdc14b445aabfa8f38a5b2a50111
ClintonYounge <clintonyounge18@gmail.com>
2023-10-03 17:30:03 -0400

    Update App.js to fetch and display Greeting

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,23 @@
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Greeting from './Greeting'
+import { useDispatch } from 'react-redux'
+import { fetchGreetings } from '../redux/greetingsSlice'
+
 const App = () => {
-  return (<h1>Hello World!</h1>);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchGreetings())
+  }, [dispatch])
+
+  return (
+
+  <Routes>
+    <Route path="/" element={<Greeting />} />
+  </Routes>
+
+  )
 }
 
-export default App;
+export default App
